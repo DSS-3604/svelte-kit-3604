a31e719d0c0be766d3e6a41384703627e9fbc0c7
SatishLawrenceMaharaj <77852350+SatishLawrenceMaharaj@users.noreply.github.com>
2023-02-03 21:40:40 -0400

    Account Page

---
+++ src/routes/account.svelte
@@ -0,0 +1,18 @@
+<script>
+    let pageName="Profile"
+</script>
+
+<div class="profile">
+    <h2>{pageName}</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus quibusdam eaque aliquid provident molestias dolores! Eius itaque fuga excepturi dolorum porro soluta doloribus recusandae, nisi libero, officiis sed, consequuntur minus.</p>
+
+</div>
+
+<style>
+.profile{
+    text-align: center;
+    display: inline;
+    margin: auto;
+    color: aliceblue;
+}
+</style>
