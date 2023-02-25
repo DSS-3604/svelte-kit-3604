aab2cdfeb36952f9483fb5dc7622a3c277ad07d2
SatishLawrenceMaharaj <77852350+SatishLawrenceMaharaj@users.noreply.github.com>
2023-02-25 18:02:44 -0400

    User Profile Page Updates

--- src/routes/my-profile/+page.svelte
+++ src/routes/my-profile/+page.svelte
@@ -1,1 +1,86 @@
-This is the users profile
+<script lang="ts">
+	import {
+		Card,
+		Button,
+		Input,
+		InputAddon,
+		ButtonGroup,
+		Chevron,
+		Dropdown,
+		DropdownItem,
+		Carousel,
+		CarouselTransition
+	} from 'flowbite-svelte';
+</script>
+
+<div class="user-account">
+	<div class="profile-imgs">
+		<div class="user-picture" style="">
+			<div class="user-pucture-background">
+				<img src="https://imagizer.imageshack.com/img921/9628/VIaL8H.jpg" alt="background-img" />
+			</div>
+			<div class="user-profile-picture ">
+				<div class="profile-pic">
+					<img
+						class="h-auto max-w-full"
+						src="https://imagizer.imageshack.com/img921/3072/rqkhIb.jpg"
+						alt="profile-pic"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="user-information" style="background-color: green;height:10px" />
+</div>
+
+<style>
+	.profile-imgs {
+		position: relative;
+	}
+	.user-picture {
+		border: 5px solid black;
+		height: 50vh;
+	}
+	.user-pucture-background {
+		height: 360px;
+		overflow: hidden;
+		z-index: 1;
+	}
+	.user-pucture-background img {
+		width: 100%;
+		margin-top: -20%;
+		filter: blur(3px);
+ 	 	-webkit-filter: blur(3px);
+	}
+	.user-profile-picture {
+		position: absolute;
+		left: 5%;
+		top: 10%;
+		bottom: 0px;
+		right: 0px;
+		height: 180px;
+		z-index: 2;
+	}
+	.profile-pic {
+		max-width: 180px;
+		max-height: 180px;
+		border-radius: 3px;
+		margin-top: 28px;
+		overflow: hidden;
+		box-shadow: 0 0 0 5px #fff;
+	}
+	.profile-pic img {
+		width: 100%;
+	}
+	@media only screen and (max-width: 600px) {
+		.profile-pic {
+			max-width: 80px;
+			max-height: 80px;
+			border-radius: 3px;
+			margin-top: 28px;
+			overflow: hidden;
+			box-shadow: 0 0 0 5px #fff;
+		}
+	}
+</style>
