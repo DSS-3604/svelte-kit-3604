44345a3e6b4834bf22756874b65c9b619a002bdb
SatishLawrenceMaharaj <77852350+SatishLawrenceMaharaj@users.noreply.github.com>
2023-02-26 14:05:45 -0400

    Update

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -19,11 +19,18 @@
 </script>
 
 <div class="flex items-center justify-center h-screen">
-    <Card class="h-2/4">
-        <div class="settings-page">
-            <div class="settings-container">
-                <h1>Account</h1>
-            </div>
-        </div>
-    </Card>
+	<Card class="h-2/4">
+		<div class="settings-page">
+			<div class="settings-container">
+				<h1>Account</h1>
+				<div class="setting-section">
+					<h2 class="settings-title">General Information</h2>
+					<div class="non-active-form">
+						<p>Name: </p>
+						<i class="fas fa-pen" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</Card>
 </div>
