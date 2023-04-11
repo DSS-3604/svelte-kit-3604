66bdf08795a1a73596ce69b051bcd519f1a416dd
SatishLawrenceMaharaj <77852350+SatishLawrenceMaharaj@users.noreply.github.com>
2023-04-10 23:06:40 -0400

    Average rating

--- src/routes/profile/[id]/+page.svelte
+++ src/routes/profile/[id]/+page.svelte
@@ -79,13 +79,20 @@
 		rankingOptions = newList;
 		review.rating = n;
 	};
-
+	let averageRating = '0';
 	onMount(async () => {
 		$utils.getFarmer(data.id).then((res) => {});
 		$utils.silentLogin().then((res) => {
 			$utils.fetchFarmerProducts(data.id).then((res) => {});
 			$utils.getFarmerReviews(data.id).then((res) => {
 				$mainStore.farmer.reviews = res;
+				let total = 0;
+				let count = 0;
+				for (let i = 0; i < $mainStore.user.reviews.length; i++) {
+					total = total + parseInt($mainStore.user.reviews[i].rating);
+					count++;
+				}
+				averageRating = (total / count).toFixed(1);
 			});
 		});
 		setActive('review');
@@ -133,7 +140,7 @@
 							d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
 						/></svg
 					>
-					<p class="text-sm text-gray-500 dark:text-gray-400">4.38</p>
+					<p class="text-sm text-gray-500 dark:text-gray-400 ml-1.5">{averageRating}</p>
 				</div>
 				<div class="flex flex-col items-center">
 					<p class="text-xl font-semibold text-gray-900 dark:text-white">
